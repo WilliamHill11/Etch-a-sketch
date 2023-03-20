ee437faa79d38e6e70a94b73ce6c51440b2dea36
william hill <williamhill0831@Hotmail.com>
2023-03-20 19:28:23 -0400

    Creating outline for project

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+const grids = document.createElement('div');
+
+grids.style.width = '16px';
+grids.style.height = '16px';
+grids.style.backgroundColor = 'blue';
+
+container.append(grids);
