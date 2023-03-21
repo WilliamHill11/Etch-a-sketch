8f80f40565f542af0e336cb4246e6aeffe6d1f21
william hill <williamhill0831@Hotmail.com>
2023-03-20 22:04:30 -0400

    Added grid layout

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const container = document.querySelector('.container');
-const grids = document.createElement('div');
 
-grids.style.width = '16px';
-grids.style.height = '16px';
-grids.style.backgroundColor = 'blue';
 
-container.append(grids);
+
+
+// Ask user the size of how big they want the sketch 
+// from 1-100 max, when click it should reset and change board size
+// let sketch = prompt('1-100');
+for (i = 0; i < 16; i++) {
+    const grids = document.createElement('div');
+    container.append(grids);
+    grids.style.width = '16';
+    grids.style.height = '16';
+    grids.style.backgroundColor = 'blue';
+    grids.style.border = 'solid black';
+    // container.style.gridTemplateColumns = 'auto * 16';
+}
+
+// reset or clear button 
+
+// rainbow color palette 
