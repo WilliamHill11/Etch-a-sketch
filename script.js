b3c7daffb979ae9c7789e6f081c0b2438c8d77d9
william hill <williamhill0831@Hotmail.com>
2023-03-22 02:16:43 -0400

    Added color sketch and css styling

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 // Ask user the size of how big they want the sketch 
 // from 1-100 max, when click it should reset and change board size
+const container = document.querySelector('.container');
+// let gridSize = prompt('Choose a grid size from 1-100');
+// for (let i = 0;  i < 16; i++) {
+//     if (gridSize > 100) {
+//         return 
+//     }
 function createGrid() {
-    const container = document.querySelector('.container');
-    let gridSize = prompt('Choose a grid size from 1-100');
-    for (let i = 0;  i < gridSize; i++) {
-        if (gridSize > 100) {
-            return 
-        }
+    for (let i = 0;  i < 16; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
         container.append(row);
-        for (let j = 0; j < gridSize; j++) {
+        for (let j = 0; j < 16; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
             row.append(cell);
+            //Hover effect to make pixelated trail in grid
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = getRandomColor();
+            })
       }
     }
   }
 
-  createGrid()
+  
+function getRandomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+createGrid()
+
 
 // reset or clear button 
 
-//Hover effect to make pixelated trail in grid
